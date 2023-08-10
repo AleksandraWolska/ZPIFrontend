9eefb4034f39d1a39c5c57b860c98f80281e0a38
janeliasz <eljasior@gmail.com>
2023-08-10 09:34:12 +0200

    Set up prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'airbnb-typescript',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
   },
-  plugins: ['react', '@typescript-eslint', 'react-refresh'],
+  plugins: ["react", "@typescript-eslint", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    'react/react-in-jsx-scope': 0,
-    'import/no-absolute-path': 0
+    "react/react-in-jsx-scope": 0,
+    "import/no-absolute-path": 0,
   },
-}
+};
