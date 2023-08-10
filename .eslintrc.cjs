c66850a141d8095d1dbd5ec76f5b5a60d05e95bf
janeliasz <eljasior@gmail.com>
2023-08-10 09:20:26 +0200

    Set up eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
+    'import/no-absolute-path': 0
   },
 }
