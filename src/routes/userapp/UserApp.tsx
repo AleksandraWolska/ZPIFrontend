24776f2423d683c0f50ae98584a88b9ade31d86c
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-09-07 09:07:43 +0200

    updated userapp main component, to switch between variants, placed header inside

--- src/routes/userapp/UserApp.tsx
+++ src/routes/userapp/UserApp.tsx
@@ -1,12 +1,32 @@
-import UserHeader from "./UserHeader";
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import UserApp1 from "./usecases/UserApp1";
-
+import UserApp2 from "./usecases/UserApp2";
+import { useState } from 'react';
+// import UserApp3 from "./usecases/UserApp3";
 
 function UserApp() {
+
+    const [activeApp, setActiveApp] = useState<number>(1);
     return (
-        <>
-            <UserHeader logoText={"COMPANY"} />
-            <UserApp1 />
+
+         <>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            COMPANY
+                        </Typography>
+                        <Button color="inherit" onClick={() => setActiveApp(1)}>UserApp1</Button>
+                        <Button color="inherit" onClick={() => setActiveApp(2)}>UserApp2</Button>
+                        {/* <Button color="inherit" onClick={() => setActiveApp(3)}>UserApp3</Button> */}
+                        <Button color="inherit">Log in</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
+            {activeApp === 1 && <UserApp1 />}
+            {activeApp === 2 && <UserApp2 />}
+            {/* {activeApp === 3 && <UserApp3 />} */}
         </>
     );
 }
