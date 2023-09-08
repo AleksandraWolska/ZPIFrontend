3390d84daab3d55f13a8593b37d9ba1834e8b8a6
janeliasz <eljasior@gmail.com>
2023-09-08 10:22:56 +0200

    Schema reducer refactor

--- src/routes/schemas/schemaReducer.ts
+++ src/routes/schemas/schemaReducer.ts
@@ -1,60 +1,13 @@
-import {
-  Schema,
-  TimeFrame,
-  Granularity,
-  UsersPerOffer,
-  GapBetween,
-  EntityUniqueness,
-  SpecificSeats,
-  Periodicity,
-  SchemaStep,
-  SCHEMA_STEPS,
-} from "./types";
+import { Schema, SchemaStep, SCHEMA_STEPS } from "./types";
 
 export const SCHEMA_ACTION_TYPES = {
-  SET_TIME_FRAME: "SET_TIME_FRAME",
-  SET_GRANULARITY: "SET_GRANULARITY",
-  SET_USERS_PER_OFFER: "SET_USERS_PER_OFFER",
-  SET_ENTITY_UNIQUENESS: "SET_ENTITY_UNIQUENESS",
-  SET_GAP_BETWEEN: "SET_GAP_BETWEEN",
-  SET_SPECIFIC_SEATS: "SET_SPECIFIC_SEATS",
-  SET_PERIODICITY: "SET_PERIODICITY",
+  APPEND_TO_SCHEMA: "APPEND_TO_SCHEMA",
   WITHDRAW: "WITHDRAW",
 } as const;
 
-type SetTimeFrameAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_TIME_FRAME;
-  payload: TimeFrame;
-};
-
-type SetGranularityAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_GRANULARITY;
-  payload: Granularity;
-};
-
-type SetUsersPerOfferAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_USERS_PER_OFFER;
-  payload: UsersPerOffer;
-};
-
-type SetEntityUniquenessAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_ENTITY_UNIQUENESS;
-  payload: EntityUniqueness;
-};
-
-type SetGapBetweenAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_GAP_BETWEEN;
-  payload: GapBetween;
-};
-
-type SetSpecificSeatsAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_SPECIFIC_SEATS;
-  payload: SpecificSeats;
-};
-
-type SetPeriodicityAction = {
-  type: typeof SCHEMA_ACTION_TYPES.SET_PERIODICITY;
-  payload: Periodicity;
+type AppendToSchemaAction = {
+  type: typeof SCHEMA_ACTION_TYPES.APPEND_TO_SCHEMA;
+  payload: Partial<Schema>;
 };
 
 type WithdrawAction = {
@@ -62,43 +15,23 @@ type WithdrawAction = {
   payload: SchemaStep;
 };
 
-type SchemaAction =
-  | SetTimeFrameAction
-  | SetGranularityAction
-  | SetUsersPerOfferAction
-  | SetEntityUniquenessAction
-  | SetGapBetweenAction
-  | SetSpecificSeatsAction
-  | SetPeriodicityAction
-  | WithdrawAction;
+type SchemaAction = AppendToSchemaAction | WithdrawAction;
 
 export function schemaReducer(
   schema: Partial<Schema>,
   action: SchemaAction,
 ): Partial<Schema> {
   switch (action.type) {
-    case SCHEMA_ACTION_TYPES.SET_TIME_FRAME:
-      return { ...schema, timeFrame: action.payload };
-    case SCHEMA_ACTION_TYPES.SET_GRANULARITY:
-      return { ...schema, granularity: action.payload };
-    case SCHEMA_ACTION_TYPES.SET_USERS_PER_OFFER:
-      return { ...schema, usersPerOffer: action.payload };
-    case SCHEMA_ACTION_TYPES.SET_ENTITY_UNIQUENESS:
-      return { ...schema, entityUniqueness: action.payload };
-    case SCHEMA_ACTION_TYPES.SET_GAP_BETWEEN:
-      return { ...schema, gapBetween: action.payload };
-    case SCHEMA_ACTION_TYPES.SET_SPECIFIC_SEATS:
-      return { ...schema, specificSeats: action.payload };
-    case SCHEMA_ACTION_TYPES.SET_PERIODICITY:
-      return { ...schema, periodicity: action.payload };
+    case SCHEMA_ACTION_TYPES.APPEND_TO_SCHEMA:
+      return { ...schema, ...action.payload };
     case SCHEMA_ACTION_TYPES.WITHDRAW:
-      return withdrawSchemaAttibutes(schema, action.payload);
+      return withdrawSchemaAttributes(schema, action.payload);
     default:
       throw Error("Unknown reducer action!");
   }
 }
 
-function withdrawSchemaAttibutes(schema: Partial<Schema>, step: SchemaStep) {
+function withdrawSchemaAttributes(schema: Partial<Schema>, step: SchemaStep) {
   switch (step) {
     case SCHEMA_STEPS.TIME_FRAME:
       return {};
