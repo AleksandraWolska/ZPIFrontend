3586177a77c8a31e3b59ab702610d7f118b942e7
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-09-19 20:49:10 +0200

    display name of company

--- src/routes/userapp/UserAppWrapper.tsx
+++ src/routes/userapp/UserAppWrapper.tsx
@@ -1,6 +1,10 @@
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { Outlet, useParams } from "react-router-dom";
+import { jsonString } from "./mocks/json_template_second_screen";
+import { FetchedJsonSecondScreen } from "./mocks/userapp_types";
 
+const jsonData: FetchedJsonSecondScreen = JSON.parse(jsonString);
+  
 function UserAppWrapper() {
   const { appId } = useParams();
   return (
@@ -9,7 +13,7 @@ function UserAppWrapper() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              COMPANY {appId}
+              {jsonData.userapp_builder_config.layoutConfig.companyName} {appId}
             </Typography>
             <Button color="inherit">Contact</Button>
             <Button color="inherit">Log in</Button>
