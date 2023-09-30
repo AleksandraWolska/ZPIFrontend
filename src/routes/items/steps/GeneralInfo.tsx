f62a9a5ebf492bf5039dd27c9cc8cc505dfa23c5
janeliasz <eljasior@gmail.com>
2023-09-30 11:58:00 +0200

    Improve styling

--- src/routes/items/steps/GeneralInfo.tsx
+++ src/routes/items/steps/GeneralInfo.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import { NewItem } from "../types";
 
 function GeneralInfo({
@@ -12,41 +12,58 @@ function GeneralInfo({
 }) {
   return (
     <>
-      <TextField
-        label="title"
-        name="title"
-        value={newItem.title}
-        onChange={(e) => setAttribute({ title: e.target.value })}
-        required
-      />
-      <TextField
-        label="subtitle"
-        name="subtitle"
-        value={newItem.subtitle}
-        onChange={(e) => setAttribute({ subtitle: e.target.value })}
-      />
-      <TextField
-        label="description"
-        name="description"
-        value={newItem.description}
-        onChange={(e) => setAttribute({ description: e.target.value })}
-        multiline
-      />
-      <TextField
-        label="image"
-        name="image"
-        value={newItem.image}
-        onChange={(e) => setAttribute({ image: e.target.value })}
-      />
-      <TextField
-        label="availableAmount"
-        name="availableAmount"
-        value={newItem.availableAmount}
-        onChange={(e) =>
-          setAttribute({ availableAmount: Number(e.target.value) })
-        }
-        type="number"
-      />
+      <Grid container spacing={1} width="50%">
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="title"
+            name="title"
+            value={newItem.title}
+            onChange={(e) => setAttribute({ title: e.target.value })}
+            fullWidth
+            required
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="subtitle"
+            name="subtitle"
+            value={newItem.subtitle}
+            onChange={(e) => setAttribute({ subtitle: e.target.value })}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="description"
+            name="description"
+            value={newItem.description}
+            onChange={(e) => setAttribute({ description: e.target.value })}
+            fullWidth
+            multiline
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="image"
+            name="image"
+            value={newItem.image}
+            onChange={(e) => setAttribute({ image: e.target.value })}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="availableAmount"
+            name="availableAmount"
+            value={newItem.availableAmount}
+            onChange={(e) =>
+              setAttribute({ availableAmount: Number(e.target.value) })
+            }
+            fullWidth
+            type="number"
+          />
+        </Grid>
+      </Grid>
 
       <button type="button" onClick={goNext}>
         Next
