9da69ca0a009c2efa4888b16e26f9048b94ee14f
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-10-01 12:26:08 +0200

    created folder for all cores not yet custom

---
+++ src/mocks/handlers/user-app/core_9/dummyOwner.ts
@@ -0,0 +1,7 @@
+export const dummyOwner = {
+  name: "TechRental Inc",
+  logoSrc: "https://example.com/techrental-logo.png",
+  phone: "+1 (123) 456-7890",
+  email: "info@techrental.com",
+};
+export default dummyOwner;
