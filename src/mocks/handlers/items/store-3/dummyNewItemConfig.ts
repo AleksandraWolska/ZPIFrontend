ef121a8b7d053188d4f4ff2e05596d748ffb7617
janeliasz <eljasior@gmail.com>
2023-10-13 14:06:56 +0200

    Create new dummy data

---
+++ src/mocks/handlers/items/store-3/dummyNewItemConfig.ts
@@ -0,0 +1,71 @@
+import { NewItemConfig } from "../../../../routes/items/types";
+
+const dummyNewItemConfig: NewItemConfig = {
+  core: {
+    flexibility: true,
+    granularity: true,
+    allowOverNight: true,
+    simultaneous: false,
+    uniqueness: true,
+  },
+  customAttributesSpec: [
+    {
+      id: "1",
+      name: "Manufacturer",
+      dataType: "string",
+      isRequired: true,
+      isFilterable: true,
+      showMainPage: true,
+      showDetailsPage: true,
+      limitValues: true,
+      possibleValues: ["Mercedes", "Audi", "BMW"],
+    },
+    {
+      id: "2",
+      name: "Model",
+      dataType: "string",
+      isRequired: true,
+      isFilterable: true,
+      showMainPage: true,
+      showDetailsPage: true,
+    },
+    {
+      id: "3",
+      name: "Year",
+      dataType: "number",
+      isRequired: false,
+      isFilterable: true,
+      showMainPage: false,
+      showDetailsPage: true,
+    },
+    {
+      id: "4",
+      name: "Passengers",
+      dataType: "number",
+      isRequired: false,
+      isFilterable: true,
+      showMainPage: true,
+      showDetailsPage: true,
+    },
+    {
+      id: "5",
+      name: "Power",
+      dataType: "number",
+      isRequired: false,
+      isFilterable: true,
+      showMainPage: false,
+      showDetailsPage: true,
+    },
+    {
+      id: "6",
+      name: "Automatic",
+      dataType: "boolean",
+      isRequired: false,
+      isFilterable: true,
+      showMainPage: true,
+      showDetailsPage: true,
+    },
+  ],
+};
+
+export default dummyNewItemConfig;
