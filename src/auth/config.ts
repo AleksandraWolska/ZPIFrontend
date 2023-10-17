ff2ce4dc3fa4e86d99e82befe668b489df20c54d
janeliasz <eljasior@gmail.com>
2023-10-17 08:44:17 +0200

    Connect to keycloak

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,9 +1,10 @@
 export const oidcConfig = {
-  authority: "http://localhost:4000/realms/myrealm",
-  client_id: "myclient",
-  redirect_uri: "http://localhost:5173/secret",
-  scope: "openid",
-  onSigninCallback: () => {
-    window.history.replaceState({}, document.title, window.location.pathname);
-  },
+  authority: "http://130.61.252.200:8180/realms/staffect",
+  client_id: "staffect",
+  redirect_uri: "http://genervation.azurewebsites.net/secret",
+  resource: "spring-app",
+  // scope: "openid",
+  // onSigninCallback: () => {
+  //   window.history.replaceState({}, document.title, window.location.pathname);
+  // },
 };
