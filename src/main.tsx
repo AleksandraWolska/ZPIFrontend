646ed7a89c81114fb1e002d376d9de959da6523f
janeliasz <eljasior@gmail.com>
2023-10-18 13:32:41 +0200

    Initialize the app in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./i18n";
+import { keycloak } from "./auth/keycloak";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const renderApp = () =>
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+
+const initializeApp = () => {
+  keycloak.init({}).then(() => {
+    renderApp();
+  });
+};
+
+initializeApp();
