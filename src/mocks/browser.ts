7979a2658f75899a928a601e9590df3bf8b8a746
janeliasz <eljasior@gmail.com>
2023-10-28 14:03:06 +0200

    Remove todos

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,10 +1,5 @@
 import { setupWorker } from "msw";
-import { todosHandlers } from "./handlers/todosHandlers";
 import { userAppHandlers } from "./handlers/user-app/userAppHandlers";
 import { adminAppHandlers } from "./handlers/admin-app/adminAppHandlers";
 
-export const worker = setupWorker(
-  ...todosHandlers,
-  ...userAppHandlers,
-  ...adminAppHandlers,
-);
+export const worker = setupWorker(...userAppHandlers, ...adminAppHandlers);
