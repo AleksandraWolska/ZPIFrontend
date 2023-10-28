bb93413193e805daaa39747b8a6a088be6108794
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-10-28 16:13:06 +0200

    flexibility

---
+++ src/routes/store-config-wizard/steps/commonStyles.tsx
@@ -0,0 +1,57 @@
+export const outerFormBox = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "100%",
+  position: "relative",
+  marginTop: "20px",
+};
+
+export const choiceButtonContainer = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  width: "100%",
+};
+
+export const titleForm = {
+  marginBottom: "10px",
+};
+
+export const descriptionForm = {
+  textAlign: "center",
+  margin: "20px",
+};
+
+export const infoCoreOuterContainer = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+};
+
+export const infoCoreBox = {
+  width: "50%",
+  padding: "20px",
+};
+
+export const infoCoreText = {
+  textAlign: "center",
+  opacity: ".7",
+};
+
+export const backIcon = {
+  top: 0,
+  left: 10,
+  position: "absolute",
+};
+export const infoIcon = {
+  top: 0,
+  right: 15,
+  position: "absolute",
+};
+
+export const choiceButton = {
+  margin: "10px",
+  width: "40%",
+};
--- src/routes/store-config-wizard/steps/core/Flexibility.tsx
+++ src/routes/store-config-wizard/steps/core/Flexibility.tsx
@@ -1,6 +1,19 @@
+/* eslint-disable react/no-unescaped-entities */
+import {
+  Box,
+  Button,
+  Collapse,
+  Divider,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import InfoIcon from "@mui/icons-material/Info";
+import ArrowBack from "@mui/icons-material/ArrowBack";
 import { STORE_CONFIG_STEPS, StoreConfigStep } from "../../types";
 import { useStoreConfig } from "../../StoreConfigProvider";
 import { calculateProgress } from "./utils";
+import * as style from "../commonStyles";
 
 function Flexibility({
   setActiveStep,
@@ -10,49 +23,85 @@ function Flexibility({
   setProgress: (progress: number) => void;
 }) {
   const { appendCoreAttribute } = useStoreConfig();
-
+  const [showInfo, setShowInfo] = useState(false);
   return (
-    <>
-      <div>Flexibility - True or False?</div>
-
-      <button
-        type="button"
+    <Box sx={style.outerFormBox}>
+      <IconButton
+        sx={style.backIcon}
         onClick={() => {
           const prevStep = STORE_CONFIG_STEPS.OWNER;
           setActiveStep(prevStep);
         }}
       >
-        BACK
-      </button>
-
-      <button
-        type="button"
-        onClick={() => {
-          appendCoreAttribute("flexibility", true);
-          const nextStep = STORE_CONFIG_STEPS.GRANULARITY;
-          setActiveStep(nextStep);
-          setProgress(
-            calculateProgress(STORE_CONFIG_STEPS.FLEXIBILITY, nextStep),
-          );
-        }}
+        <ArrowBack />
+      </IconButton>
+      <IconButton
+        sx={style.infoIcon}
+        size="small"
+        onClick={() => setShowInfo(!showInfo)}
       >
-        True
-      </button>
-
-      <button
-        type="button"
-        onClick={() => {
-          appendCoreAttribute("flexibility", false);
-          const nextStep = STORE_CONFIG_STEPS.SIMULTANEOUS;
-          setActiveStep(nextStep);
-          setProgress(
-            calculateProgress(STORE_CONFIG_STEPS.FLEXIBILITY, nextStep),
-          );
-        }}
-      >
-        False
-      </button>
-    </>
+        <InfoIcon />
+      </IconButton>
+      <Typography variant="h4" sx={style.titleForm}>
+        Time frame flexibility
+      </Typography>
+      <Box margin="10px">
+        <Typography sx={style.descriptionForm}>
+          Select whether an item's reservation timeframe should be predetermined
+          and fixed, or if the user has the flexibility to choose according to
+          their preferences.
+        </Typography>
+        <Collapse in={showInfo} timeout="auto" unmountOnExit>
+          <Box sx={style.infoCoreOuterContainer}>
+            <Box sx={style.infoCoreBox}>
+              <Typography sx={style.infoCoreText}>
+                Flexible value means that the item can be reserved for a period
+                chosen by user within specified time frames.
+              </Typography>
+            </Box>
+            <Divider orientation="vertical" flexItem />
+            <Box sx={style.infoCoreBox}>
+              <Typography sx={style.infoCoreText}>
+                Fixed value indicates that the item has fixed start and end
+                times set by an admin, and users can only sign up for the entire
+                duration.
+              </Typography>
+            </Box>
+          </Box>
+        </Collapse>
+      </Box>
+      <Box sx={style.choiceButtonContainer}>
+        <Button
+          sx={style.choiceButton}
+          variant="contained"
+          onClick={() => {
+            appendCoreAttribute("flexibility", true);
+            const nextStep = STORE_CONFIG_STEPS.GRANULARITY;
+            setActiveStep(nextStep);
+            setProgress(
+              calculateProgress(STORE_CONFIG_STEPS.FLEXIBILITY, nextStep),
+            );
+          }}
+        >
+          Flexible
+        </Button>
+        <Button
+          sx={style.choiceButton}
+          type="button"
+          variant="contained"
+          onClick={() => {
+            appendCoreAttribute("flexibility", false);
+            const nextStep = STORE_CONFIG_STEPS.SIMULTANEOUS;
+            setActiveStep(nextStep);
+            setProgress(
+              calculateProgress(STORE_CONFIG_STEPS.FLEXIBILITY, nextStep),
+            );
+          }}
+        >
+          Fixed
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
