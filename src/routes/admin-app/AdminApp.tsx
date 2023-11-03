86bb39275c72aed139eb6338dfe43c2bbc7e5976
janeliasz <eljasior@gmail.com>
2023-11-03 15:05:39 +0100

    Style admin panel

--- src/routes/admin-app/AdminApp.tsx
+++ src/routes/admin-app/AdminApp.tsx
@@ -1,12 +1,61 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import FormatListBulletedSharpIcon from "@mui/icons-material/FormatListBulletedSharp";
+import CreateSharpIcon from "@mui/icons-material/CreateSharp";
+import { Box, Container, Grid, Paper, Stack, Typography } from "@mui/material";
+
+const options = [
+  {
+    label: "Item list",
+    value: "item-list",
+    icon: <FormatListBulletedSharpIcon sx={{ fontSize: "5rem" }} />,
+  },
+  {
+    label: "New item",
+    value: "add-item",
+    icon: <CreateSharpIcon sx={{ fontSize: "5rem" }} />,
+  },
+];
 
 function AdminApp() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Link to="item-list">Item list</Link>
-      <br />
-      <Link to="add-item">New item</Link>
-    </>
+    <Container maxWidth="lg">
+      <Grid container spacing={2} marginTop={4}>
+        {options.map((option) => {
+          return (
+            <Grid
+              key={option.value}
+              item
+              xs={6}
+              md={3}
+              sx={{ cursor: "pointer" }}
+            >
+              <Paper
+                onClick={() => navigate(option.value)}
+                sx={{ paddingY: 1 }}
+              >
+                <Stack
+                  height="100%"
+                  alignItems="center"
+                  justifyContent="center"
+                  gap={3}
+                >
+                  <Box>{option.icon}</Box>
+                  <Typography
+                    fontSize="1.75rem"
+                    letterSpacing="1px"
+                    textTransform="uppercase"
+                  >
+                    {option.label}
+                  </Typography>
+                </Stack>
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 }
 
--- src/routes/admin-app/AdminAppWrapper.tsx
+++ src/routes/admin-app/AdminAppWrapper.tsx
@@ -1,32 +1,14 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import useStoreConfig from "./useStoreConfig";
 import StoreConfigWizard from "../store-config-wizard/StoreConfigWizard";
+import TopBar from "./TopBar";
 
 function AdminAppWrapper() {
   const storeConfig = useStoreConfig();
 
   return storeConfig ? (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: "2rem",
-        }}
-      >
-        <h1>Admin App</h1>
-        <div
-          style={{
-            display: "flex",
-            gap: "1rem",
-          }}
-        >
-          <Link to="/">Home</Link>
-          <Link to="item-list">Item list</Link>
-          <Link to="add-item">Add item</Link>
-        </div>
-      </div>
-
+      <TopBar />
       <Outlet />
     </>
   ) : (
