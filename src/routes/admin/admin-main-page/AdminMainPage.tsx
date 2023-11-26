9e70ba3d750d8812c46b418ab5cba709044a2440
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-11-26 14:49:10 +0100

    admin main page improved ui

--- src/routes/admin/admin-main-page/AdminMainPage.tsx
+++ src/routes/admin/admin-main-page/AdminMainPage.tsx
@@ -1,24 +1,108 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import StoreIcon from "@mui/icons-material/Store";
+import AddBusinessIcon from "@mui/icons-material/AddBusiness";
 import useAdminStores from "./useAdminStores";
 import { StoreSummary } from "../../../types";
 
 function AdminMainPage() {
   const adminStores = useAdminStores() as StoreSummary[];
+  const navigate = useNavigate();
+  const theme = useTheme();
   return (
-    <div>
-      <Link to="new">New store</Link>
-      <br />
-      <br />
+    <Container>
+      {adminStores.length && (
+        <Typography m={2} variant="h4">
+          Your stores
+        </Typography>
+      )}
 
-      {adminStores.map((adminStore) => {
-        return (
-          <div key={adminStore.storeConfigId}>
-            <Link to={adminStore.storeConfigId}>{adminStore.name}</Link>
-            <br />
-          </div>
-        );
-      })}
-    </div>
+      <List>
+        {adminStores.map((adminStore) => {
+          return (
+            <ListItem
+              key={adminStore.storeConfigId}
+              onClick={() => navigate(adminStore.storeConfigId)}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  width: "100%",
+                  boxShadow: 3,
+                  borderRadius: "10px",
+                  padding: 2,
+                  bgcolor: "white",
+                  cursor: "pointer",
+                  "&:hover": {
+                    bgcolor: theme.palette.action.hover,
+                  },
+                }}
+              >
+                <Box sx={{ margin: 1, marginRight: 3 }}>
+                  <StoreIcon sx={{ fontSize: "5rem", color: "grey" }} />
+                </Box>
+                <ListItemText
+                  primary={
+                    <Typography variant="h4">{adminStore.name}</Typography>
+                  }
+                  secondary={
+                    <Typography variant="body1" color="grey">
+                      Manage your items, reservation in already existing store
+                    </Typography>
+                  }
+                />
+              </Box>
+            </ListItem>
+          );
+        })}
+      </List>
+      <Typography m={2} mt={4} mb={3} variant="h4">
+        {adminStores.length
+          ? "Or create new store?"
+          : "You have no stores yet, start with creating one"}
+      </Typography>
+
+      <ListItem key="new" onClick={() => navigate("new")}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            boxShadow: 3,
+            borderRadius: "10px",
+            padding: 2,
+            bgcolor: "white",
+            cursor: "pointer",
+            "&:hover": {
+              bgcolor: theme.palette.action.hover,
+            },
+          }}
+        >
+          <Box sx={{ margin: 1, marginRight: 3 }}>
+            <AddBusinessIcon sx={{ fontSize: "5rem", color: "grey" }} />
+          </Box>
+          <ListItemText
+            primary={<Typography variant="h4">New store</Typography>}
+            secondary={
+              <Typography variant="body1" color="grey">
+                Create new store for your business
+              </Typography>
+            }
+          />
+        </Box>
+      </ListItem>
+    </Container>
   );
 }
 
