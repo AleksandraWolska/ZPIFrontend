6fef3c3403b8c38bbd69f4e869b1cc586847d0be
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-11-27 19:30:56 +0100

    change backend endpoint for checkAvailability

--- src/routes/userapp/details-page/useAvailabilityCheck.tsx
+++ src/routes/userapp/details-page/useAvailabilityCheck.tsx
@@ -1,14 +1,16 @@
+import { useParams } from "react-router-dom";
 import { useMutation } from "react-query";
 import { CheckAvailabilityRequest } from "../types";
 import { BACKEND_URL } from "../../../query";
 
 function useAvailabilityCheck() {
+  const params = useParams() as { storeId: string };
   const mutation = useMutation((data: CheckAvailabilityRequest) =>
     fetch(
       `${
         process.env.NODE_ENV === "development"
           ? `/api/check-availability`
-          : `${BACKEND_URL}/check-availability`
+          : `${BACKEND_URL}/stores/${params.storeId}/check`
       }`,
       {
         method: "POST",
