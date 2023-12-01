40887a0156ad443a98a31d10cc79c8865ca5ba85
janeliasz <eljasior@gmail.com>
2023-12-01 20:07:49 +0100

    Add userapp cypress tests

--- src/mocks/data/store-998/items.ts
+++ src/mocks/data/store-998/items.ts
@@ -4,7 +4,45 @@ export const items: Item[] = [
   {
     id: "1",
     attributes: {
-      title: "Rock festival 2024",
+      title: "Our unforgettable pop music festival!",
+      subtitle: "",
+      description: "",
+      image:
+        "https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    customAttributeList: [
+      {
+        id: "1",
+        name: "Artist",
+        value: "Maryla Rodowicz",
+      },
+      {
+        id: "2",
+        name: "Genre",
+        value: "Pop",
+      },
+      {
+        id: "3",
+        name: "Price",
+        value: 100,
+      },
+      {
+        id: "4",
+        name: "Adults only",
+        value: false,
+      },
+    ],
+    amount: 600,
+    schedule: {
+      startDateTime: "2024-02-03T12:00:00Z",
+    },
+    active: true,
+    availableAmount: 600,
+  },
+  {
+    id: "2",
+    attributes: {
+      title: "Rock Festival 2024",
       subtitle: "",
       description: "",
       image:
@@ -35,44 +73,6 @@ export const items: Item[] = [
     amount: 500,
     schedule: {
       startDateTime: "2024-02-02T12:00:00Z",
-    },
-    active: true,
-    availableAmount: 500,
-  },
-  {
-    id: "2",
-    attributes: {
-      title: "Unforgettable pop music festival!",
-      subtitle: "",
-      description: "",
-      image:
-        "https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-    customAttributeList: [
-      {
-        id: "1",
-        name: "Artist",
-        value: "Maryla Rodowicz",
-      },
-      {
-        id: "2",
-        name: "Genre",
-        value: "Pop",
-      },
-      {
-        id: "3",
-        name: "Price",
-        value: 100,
-      },
-      {
-        id: "4",
-        name: "Adults only",
-        value: false,
-      },
-    ],
-    amount: 300,
-    schedule: {
-      startDateTime: "2024-02-03T12:00:00Z",
     },
     active: true,
     availableAmount: 500,
