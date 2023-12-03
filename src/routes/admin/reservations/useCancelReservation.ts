519d118717a45358eac169edae994829bf30ea27
janeliasz <eljasior@gmail.com>
2023-12-03 18:12:14 +0100

    Fix rezervation api urls

--- src/routes/admin/reservations/useCancelReservation.ts
+++ src/routes/admin/reservations/useCancelReservation.ts
@@ -1,17 +1,20 @@
 import { useMutation } from "react-query";
 import { useParams } from "react-router-dom";
 import { getAccessToken } from "../../../auth/utils";
-import { queryClient } from "../../../query";
+import { BACKEND_URL, queryClient } from "../../../query";
 
 const cancelReservation = (storeId: string, reservationId: string) => {
   const token = getAccessToken();
 
-  return fetch(`/api/stores/${storeId}/reservations/${reservationId}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
+  return fetch(
+    `${BACKEND_URL}/stores/${storeId}/reservations/${reservationId}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     },
-  });
+  );
 };
 
 function useCancelReservation() {
