3fb05c9536dd2e5243df10710d10cc7e86dcd0dc
janeliasz <eljasior@gmail.com>
2023-12-03 18:12:14 +0100

    Fix rezervation api urls

--- src/routes/admin/reservations/useConfirmReservation.tsx
+++ src/routes/admin/reservations/useConfirmReservation.tsx
@@ -1,17 +1,20 @@
 import { useMutation } from "react-query";
 import { useParams } from "react-router-dom";
 import { getAccessToken } from "../../../auth/utils";
-import { queryClient } from "../../../query";
+import { BACKEND_URL, queryClient } from "../../../query";
 
 const confirmReservation = (storeId: string, reservationId: string) => {
   const token = getAccessToken();
 
-  return fetch(`/api/stores/${storeId}/reservations/${reservationId}/confirm`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${token}`,
+  return fetch(
+    `${BACKEND_URL}/stores/${storeId}/reservations/${reservationId}/confirm`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     },
-  });
+  );
 };
 
 function useConfirmReservation() {
